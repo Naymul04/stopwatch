f8e6cfd83b109a2d1ad7282a8d5df7736e48bc3e
Naymul04 <68547750+Naymul04@users.noreply.github.com>
2020-07-20 23:38:37 +0600

    Update and rename README.md to stopwatch.py

---
+++ stopwatch.py
@@ -0,0 +1,13 @@
+# stopwatch
+
+print("enter number of seconds")
+import time
+a=int(input())
+while a!=0:
+  print(str(a) + "  seconds remaining")
+  time.sleep(1)
+  a=a-1
+print ("the time is up ")
+time.sleep(1)
+
+print (".......Thank you.......")
